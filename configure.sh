a569c06727c06f3837fc67a4eef472e2b293cc50
Tom Windels <tom.windels@ugent.be>
2024-10-21 13:53:15 +0200

    Configuration script tweaks

--- configure.sh
+++ configure.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ $(ls ./**/*.nt 2> /dev/null) ]] || [[ $* == -f ]]; then
+  printf "Benchmarks appear to be configured. You can force configuration by passing \`-f\` as an argument.\nExiting...\n"
+  exit
+fi
 
 cd watdiv || exit 255
 tar xvf dataset.tar.gz
