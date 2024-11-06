fb68f6c5428b118e86873af71ba89b39860e58b2
Tom Windels <tom.windels@outlook.com>
2024-11-06 11:22:07 +0100

    Benchmark execution improvements
    Added jvm and js build steps so the benchmark tool can be built on its own
    Bumped linked tesserakt version to support benchmarking using the dedicated module

--- run.sh
+++ run.sh
@@ -1,8 +1,55 @@
-#!/bin/sh
-./configure.sh
+#!/bin/bash
 
-WATDIV="$(pwd)/watdiv"
-RAILWAY="$(pwd)/railway"
-cd ./tesserakt || exit 255
-# ./gradlew benchmarking:jvmRun --args="$WATDIV/dataset.nt $WATDIV/queries/S1.txt" -DmainClass=Main_jvmKt --quiet --stacktrace
-./gradlew benchmarking:jvmRun --args="$RAILWAY/railway-batch-1-inferred.ttl $RAILWAY/query.txt" -DmainClass=Main_jvmKt --quiet --stacktrace
+ROOT=$(pwd)
+
+prepare() {
+    # git submodule update --init --recursive
+    cd tesserakt || exit 255
+}
+
+build_jvm() {
+    if [[ -f benchmark/jvm.jar ]]; then
+        echo "JVM benchmark detected, skipping build..."
+        return
+    fi
+    prepare
+    ./gradlew benchmarking:jvmJar
+    cd "$ROOT" || exit 255
+    mkdir -p benchmark
+    mv tesserakt/benchmarking/build/libs/benchmarking-jvm-*.jar benchmark/jvm.jar
+    echo "Java benchmark ready!"
+}
+
+build_js() {
+    if [[ -L benchmark/nodejs ]]; then
+        echo "NodeJS benchmark detected, skipping build..."
+        return
+    fi
+    prepare
+    ./gradlew benchmarking:build
+    mv benchmarking/build/compileSync/js/main/productionExecutable/kotlin build/js
+    cd "$ROOT" || exit 255
+    mkdir -p benchmark
+    ln -s ../tesserakt/build/js benchmark/nodejs
+    echo "NodeJS benchmark ready!"
+}
+
+run_jvm() {
+    java -jar benchmark/jvm.jar "${@}"
+}
+
+run_js() {
+    node benchmark/nodejs/kotlin/tesserakt-benchmarking.js "${@}"
+}
+
+case "$1" in
+    "jvm")
+        build_jvm
+        run_jvm "${@:2}"
+    ;;
+    "js")
+        build_js
+        run_js "${@:2}"
+    ;;
+esac
+
