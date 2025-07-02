1f3200f21cd8d210190149e1a16f727a0eff8301
Tom Windels <tom.windels@outlook.com>
2025-07-02 10:48:58 +0200

    General script improvements
    Made the java binary used in the various endpoints configurable, so
    switching between JVM implementation (& version) can be done globally
    Applied several spellcheck fixes

--- startscripts/config.sh
+++ startscripts/config.sh
@@ -1,5 +1,11 @@
+#! /bin/bash
+
 # can be kept empty / put in comment if no initial endpoint data is required
 export ENDPOINT_DATA=../watdiv/dataset.nt
 # whether supported engines should have their caching strategy enabled - 0 or 1
 export ENABLE_QUERY_CACHE=0
-export JAVA_FLAGS="-Xmx16g"
+
+## JAVA CONFIGURATION
+export JAVA_BIN=/usr/lib/jvm/default-runtime/bin/java
+export JAVA_FLAGS="-Xmx1g"
+#export JAVA_FLAGS="-server -Xmx16g"
