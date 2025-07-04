ed1df9fba6e0d25ee868a3476854f93974e37557
Tom Windels <tom.windels@ugent.be>
2025-07-04 17:21:55 +0200

    Oxigraph timing fix
    Fixed an issue that could cause the oxigraph setup to fail because of
    the background launch of the binary, causing the cURL command to fail
    and the application to hang indefinitely; now, the port being in use
    logic is extracted and used inside the oxigraph script before attempting
    to insert the data

--- scripts/endpoints/common.sh
+++ scripts/endpoints/common.sh
@@ -7,16 +7,23 @@ on_exit_requested() {
 	echo "Exit requested, shutting down..."
 }
 
-on_endpoint_ready() {
-	echo "Waiting until the port ${ENDPOINT_PORT} becomes available"
+await_port_in_use() {
+	echo "Waiting until the port ${ENDPOINT_PORT} is in use"
 	sleep 2
 	{
-	  while ! echo -n > /dev/tcp/localhost/${ENDPOINT_PORT}; do
-	    echo -n "."
-	    sleep 2
-	  done
+		while ! echo -n > /dev/tcp/localhost/${ENDPOINT_PORT}; do
+			echo -n "."
+			sleep 2
+		done
 	} 2>/dev/null
+	echo "Port ${ENDPOINT_PORT} is in use!"
+}
 
+on_endpoint_ready() {
+	# Making sure the endpoint is actually present on the port number we have
+	# defined in the env variable
+	await_port_in_use
+	
 	trap "echo;on_exit_requested" SIGINT
 
 	printf "\nEndpoint is ready: %s\n" "$1"
