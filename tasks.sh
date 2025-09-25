9474152e9885dc31bede1b766ad128e3700fc535
Tom Windels <tom.windels@outlook.com>
2025-09-25 15:31:13 +0200

    Updated the task collection
    Added the growing dataset test set based on the BSBM input configuration
    Removed the regular benchmark evaluation (both WatDiv & BSBM)

--- tasks.sh
+++ tasks.sh
@@ -20,11 +20,6 @@ run() {
 }
 
 # Actual task list
-## Regular tests
-### WatDiv (no cache variants included)
-run python benchmark.py regular -i input/watdiv/datasets -o output/regular/watdiv -q input/watdiv/queries --runs 100 --filter "^(?:(?!-cache).)*$"
-### BSBM (no cache variants included)
-run python benchmark.py bsbm -i input/bsbm/datasets -u bin/bsbm-tools/usecases/explore/sparql.txt -o output/bsbm --filter "^(?:(?!-cache).)*$"
 ## Replay tests (cache variants included)
 run python benchmark.py replay -i input/replay -o output/replay --runs 100
 ## Memory range tests (no cache variants included)
@@ -32,6 +27,9 @@ run python benchmark.py replay -i input/replay -o output/replay --runs 100
 run python benchmark.py regular -i input/watdiv/datasets -o output/memory/watdiv -q input/watdiv/queries --memory-range 100,10000 -f "(^tesserakt$)|(^jena$)"
 ### BSBM
 run python benchmark.py regular -i input/bsbm/datasets -o output/memory/bsbm -q input/bsbm/queries --memory-range 100,10000 -f "(^tesserakt$)|(^jena$)"
+## Growing tests (cache variants included)
+### BSBM
+run python benchmark.py growing -i input/bsbm/growing -q input/bsbm/queries -o output/growing/bsbm --runs 100
 
 # The entire output can now be compressed into a single tar
 tar -czf "output-$DATE.tar.gz" output
