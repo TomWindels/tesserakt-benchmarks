9bf96702b05e8d13c0b12f444215adcc3b503e1e
Tom Windels <tom.windels@outlook.com>
2025-10-08 15:49:12 +0200

    Updated task list
    Updated the task list, replacing the `growing` task with the new `update` task and dropping the watdiv evaluation

--- tasks.sh
+++ tasks.sh
@@ -21,15 +21,13 @@ run() {
 
 # Actual task list
 ## Replay tests (cache variants included)
-run python benchmark.py replay -i input/replay -o output/replay --runs 100
+run python benchmark.py replay -i input/replay -o output/replay --runs 50
 ## Memory range tests (no cache variants included)
-### WatDiv
-run python benchmark.py regular -i input/watdiv/datasets -o output/memory/watdiv -q input/watdiv/queries --memory-range 100,10000 -f "(^tesserakt$)|(^jena$)"
 ### BSBM
 run python benchmark.py regular -i input/bsbm/datasets -o output/memory/bsbm -q input/bsbm/queries --memory-range 100,10000 -f "(^tesserakt$)|(^jena$)"
 ## Growing tests (cache variants included)
 ### BSBM
-run python benchmark.py growing -i input/bsbm/growing -q input/bsbm/queries -o output/growing/bsbm --runs 100
+run python benchmark.py update --warmup-queries input/bsbm/warmup-queries --warmup-runs 3 -i input/bsbm/update -q input/bsbm/queries -o output/update/bsbm --runs 50
 
 # The entire output can now be compressed into a single tar
 tar -czf "output-$DATE.tar.gz" output
