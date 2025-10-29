428bee2b1a0ed4abbbb2d9edc8310e20fd484d3d
Tom Windels <tom.windels@outlook.com>
2025-10-29 09:06:27 +0100

    Added result saving for incremunica replay evaluation

--- incremunica-replay-evaluation/main.js
+++ incremunica-replay-evaluation/main.js
@@ -3,6 +3,7 @@ const RDFJS = require('@tesseraktjs/testing-tooling-replay-benchmark').RDFJS;
 const fs = require('fs');
 const incremunica = require('@incremunica/query-sparql-incremental');
 const { workerData, isMainThread, Worker } = require('worker_threads');
+const { basename } = require('path');
 const StreamingStore = require('@incremunica/streaming-store').StreamingStore;
 const isAddition = require('@incremunica/user-tools').isAddition;
 
@@ -47,13 +48,13 @@ async function observe_results(total, bindingsStream) {
     const start = process.hrtime.bigint();
     let latest = start;
     // observing the changes
-    let insertions = 0;
+    let additions = 0;
     let deletions = 0;
     new Promise(() => {
         bindingsStream.on('data', (binding) => {
             latest = process.hrtime.bigint();
             if (isAddition(binding)) {
-                insertions++;
+                additions++;
                 total.set(binding, (total.get(binding) || 0) + 1)
             } else {
                 deletions++;
@@ -67,11 +68,18 @@ async function observe_results(total, bindingsStream) {
     bindingsStream.removeAllListeners('data');
     const duration = Number(latest - start) / 1_000_000;
     const checksum = get_checksum_for_resultset(total);
-    console.log(`|`, insertions, `added,`, deletions, `deleted\n| Checksum value is`, checksum, `\n> Processing took`, duration, `ms`);
-    // TODO: return the duration, insertions, deletions, and the checksum
+    console.log(`|`, additions, `added,`, deletions, `deleted\n| Checksum value is`, checksum, `\n> Processing took`, duration, `ms`);
+    return {
+        additions: additions,
+        deletions: deletions,
+        checksum: checksum,
+        duration: duration,
+    }
 }
 
 async function eval_query(query, replayer) {
+    const results = [];
+
     const store = new StreamingStore();
     const engine = new incremunica.QueryEngine();
     const stream = await engine.queryBindings(query, { sources: [store] });
@@ -94,39 +102,68 @@ async function eval_query(query, replayer) {
         deletions.forEach((quad) => {
             store.removeQuad(quad);
         });
-        // TODO: collect the results
-        await observe_results(total, stream);
+        results.push(await observe_results(total, stream));
     }
-    // TODO: return the collected results
+    return results;
 }
 
-async function eval_replayer(filename, replayer) {
+function get_outputs_csv(results) {
+    let result = 'name,additions,removals,checksum'
+    for (let i = 0; i < results.length; ++i) {
+        result += `\nΔ${i},${results[i].additions},${results[i].deletions},${results[i].checksum}`
+    }
+    return result
+}
+
+function get_timings_csv(results) {
+    let result = 'name,duration(ms)'
+    for (let i = 0; i < results.length; ++i) {
+        result += `\nΔ${i},${results[i].duration}`
+    }
+    return result
+}
+
+function save_results(output_filepath, results) {
+    fs.mkdirSync(output_filepath, { recursive: true });
+    fs.writeFileSync(`${output_filepath}/outputs.csv`, get_outputs_csv(results));
+    fs.writeFileSync(`${output_filepath}/time.csv`, get_timings_csv(results));
+}
+
+async function eval_replayer(filename, iteration, replayer) {
+    let query_index = 0;
     for (const query of replayer.queries) {
         console.log(`Evaluating query: ${query}`);
         // TODO store the results
-        await eval_query(query, replayer);
+        const results = await eval_query(query, replayer);
+        save_results(
+            `output/${basename(filename)}/query_${query_index}/run-${iteration}`,
+            results,
+        )
+        ++query_index;
     }
     // TODO write the results to an appropriate location based on filename & cwd
 }
 
 async function eval(files) {
-    for (const file of files) {
-        // starting a worker per file
-        const worker = new Worker(__filename, {workerData: file});
-        console.log(`Replaying file: ${file}`);
-        worker.on('message', (message) => {
-            console.log(`Got ${message}`);
-        });
-        await new Promise((resolve) => {
-            worker.on('error', (a) => {
-                console.log(`Received`, a, `in \`error\` request!`);
-                resolve()
+    for (let i = 0; i < 5; ++i) {        
+        for (const file of files) {
+            // starting a worker per file
+            const worker = new Worker(__filename, {workerData: {filepath: file, run: i}});
+            console.log(`Replaying file: ${file}`);
+            worker.on('message', (message) => {
+                console.log(`Got ${message}`);
             });
-            worker.on('exit', (a) => {
-                console.log(`Received`, a, `in \`exit\` request!`);
-                resolve()
+            await new Promise((resolve) => {
+                worker.on('error', (a) => {
+                    console.log(`Received`, a, `in \`error\` request!`);
+                    resolve()
+                });
+                worker.on('exit', (a) => {
+                    console.log(`Received`, a, `in \`exit\` request!`);
+                    resolve()
+                });
             });
-        });
+        }
     }
 }
 
@@ -143,10 +180,11 @@ if (isMainThread) {
     });
 } else {
     if (workerData != undefined) {
-        const filename = workerData;
-        console.log(`Worker started for file ${filename}`)
-        const replay = ReplayBenchmarkReplayer.fromFile(filename);
-        eval_replayer(filename, replay);
+        console.log(workerData)
+        const { filepath, run } = workerData;
+        console.log(`Worker started for file ${filepath}`)
+        const replay = ReplayBenchmarkReplayer.fromFile(filepath);
+        eval_replayer(filepath, run, replay);
     } else {
         console.error("No worker data available!");
         exit(1);
