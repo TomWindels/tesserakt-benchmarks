4a23236175cd05375cc80d824e147eb64489c7da
Tom Windels <tom.windels@outlook.com>
2025-11-13 20:41:48 +0100

    Removed endpoint related evaluation

--- scripts/endpoints/config.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-## JAVA CONFIGURATION
-export JAVA_BIN="${JAVA_HOME:-/usr/lib/jvm/default-runtime}"/bin/java
-export JAVA_FLAGS="${JAVA_FLAGS:--Xmx1g}"
-#export JAVA_FLAGS="-server -Xmx16g"
