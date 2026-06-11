9adbd6ccfc86d3a43641a5ade6fa6af8d6d31ca9
Tom Windels <tom.windels@outlook.com>
2026-06-11 14:52:15 +0200

    Migrate `tesserakt-jar` to KMP hierarchy & shadow plugin
    When testing an upstream version, a `kotlin("jvm")` project does not properly fetch the non-Android version of the code base, causing issues when building the Jar file
    The regular Jar method did not include all classes when using repository-sourced tesserakt version; has been fixed by using `shadow` plugin & `shadowJar` task

--- tesserakt-jar/build.gradle.kts
+++ tesserakt-jar/build.gradle.kts
@@ -1,35 +1,34 @@
 import java.util.Properties
 
 plugins {
-    kotlin("jvm") version "2.2.20"
+    kotlin("multiplatform") version "2.2.20"
+    id("com.gradleup.shadow") version "9.2.2"
 }
 
 group = "dev.tesserakt.bench"
 //version = gradle.includedBuild("tesserakt")
 version = Properties()
-    .apply { load(File(gradle.includedBuild("tesserakt").projectDir, "gradle.properties").inputStream()) }
-    .get("VERSION_NAME")
+    .runCatching {
+        load(File(gradle.includedBuild("tesserakt").projectDir, "gradle.properties").inputStream());
+        this
+    }
+    .getOrNull()
+    ?.get("VERSION_NAME")
     ?: "unknown"
+
+kotlin {
+    jvm()
+
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.github.tomwindels:tesserakt-sparql:0.3.1")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("io.github.tomwindels:tesserakt-sparql:0.3.1")
-}
-
-val jar by tasks.getting(Jar::class) {
-    // not required here
-//    manifest {
-//        attributes["Main-Class"] = "Engine"
-//    }
-
-    from(
-        configurations
-            .runtimeClasspath
-            .get()
-            .files
-            .map { if (it.isDirectory) it else zipTree(it) }
-    )
-}
